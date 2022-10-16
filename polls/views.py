d99b1b293231e914f2e0ec26d68801b5bec17b76
sklgrz <kozhievnikov02@mail.ru>
2022-10-17 01:07:52 +0300

    change index, add latest_question_list

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
@@ -14,4 +17,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("You're voting on questions %s." % question_id)
-	
+
